4a086baccbe3352dd623fdbe6a1eafa823f6bc87
DevJoanderson <joandersonsouza410@gmail.com>
2025-10-02 23:34:32 -0300

    feat: criar projeto DevSorteio com inputs, validação e sorteio de número aleatório

---
+++ script.js
@@ -0,0 +1,18 @@
+function handleDrawClick() {
+    const min = Math.ceil(Number(document.querySelector(".input-min").value));
+    const max = Math.floor(Number(document.querySelector(".input-max").value));
+
+    const mensagemErro = document.querySelector("#mensagem-erro");
+    const resultado = document.querySelector("#resultado");
+
+    mensagemErro.textContent = "";
+    resultado.textContent = "";
+
+    if (isNaN(min) || isNaN(max) || min >= max) {
+        mensagemErro.textContent = "⚠️ Valores inválidos!";
+        return;
+    }
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    resultado.textContent = `Número sorteado: ${result}`;
+}
